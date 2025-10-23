397e4fb2278238227d732ed66e2c2d4d77982311
Raul_Herrera <Raul_Herrera@empress.com.mx>
2025-10-23 16:52:25 -0600

    cambios en layout

--- EstacionGeoespacial/Program.cs
+++ EstacionGeoespacial/Program.cs
@@ -1,9 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -20,6 +24,5 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
 
 app.Run();
